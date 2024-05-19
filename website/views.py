a4c12e36b7e2c0a6a67c4c51598404032648eb56
ERICK OMONDI AYARA <ayaraerick@gmail.com>
2024-05-19 21:42:23 +0300

    first commit

--- website/views.py
+++ website/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import WhoWeAre
 from .models import QuickFacts
+from .models import ServiceIntro
 
 
 def home(request):
     whoweare = WhoWeAre.objects.all()
     quickfacts = QuickFacts.objects.all()
-    return render(request, 'index.html', {'whoweare': whoweare, 'quickfacts': quickfacts})
+    serviceintro = ServiceIntro.objects.all()
+    return render(request, 'index.html', {'whoweare': whoweare, 'quickfacts': quickfacts, 'serviceintro': serviceintro})
