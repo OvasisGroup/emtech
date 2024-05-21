da6bd468d60c512a9a4e623ee016b2b07f33d5d6
ERICK OMONDI AYARA <ayaraerick@gmail.com>
2024-05-21 11:09:43 +0300

    footer

--- website/admin.py
+++ website/admin.py
@@ -8,11 +8,20 @@ from .models import WhoWeAre
 from import_export.admin import ImportExportModelAdmin
 
 
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ("title", "body")
+
+admin.site.register(About, AboutAdmin)
+
+
+
 # Register your models here.
-admin.site.register(About, ImportExportModelAdmin)
+
 admin.site.register(ServiceIntro, ImportExportModelAdmin)
 admin.site.register(Services, ImportExportModelAdmin)
 admin.site.register(Clients, ImportExportModelAdmin)
 admin.site.register(QuickFacts, ImportExportModelAdmin)
 admin.site.register(WhoWeAre, ImportExportModelAdmin)
+
+
 
